b2fe9d40047a928dcfb2078d77578834cf0b2a68
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2024-03-30 18:40:56 -0300

    fix: first settings

--- page/src/pages/HomePage.jsx
+++ page/src/pages/HomePage.jsx
@@ -2,34 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Styled from 'styled-components';
 import SideBar from '../components/sidebar/SideBar';
 import MarkdownRenderer from '../components/renderizarMD';
+
 import heatmap from "../../docs/planejamento/heatmap.md";
 
 const HomePage = () => {
-  const [markdownContent, setMarkdownContent] = useState('');
-
-  useEffect(() => {
-    const fetchMarkdownContent = async () => {
-      try {
-        // Simulação de carregamento do arquivo Markdown
-        const response = await fetch(heatmap);
-        if (!response.ok) {
-          throw new Error('Falha ao carregar o arquivo Markdown');
-        }
-        const markdown = await response.text();
-        setMarkdownContent(markdown);
-      } catch (error) {
-        console.error('Erro ao carregar o arquivo Markdown:', error);
-      }
-    };
-
-    fetchMarkdownContent();
-  }, []);
-
   return (
     <Container>
       <SideBar/>
       <Align>
-        <MarkdownRenderer markdown={markdownContent} />
+        <MarkdownRenderer filePath={heatmap} />
       </Align>
     </Container>
   );
